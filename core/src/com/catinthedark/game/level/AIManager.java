8897ced8a10b62d2195b131f66c119f817eba5f2
over <alex64-mail.ru@mail.ru>
2014-08-24 17:28:17 +0700

    many many many

--- core/src/com/catinthedark/game/level/AIManager.java
+++ core/src/com/catinthedark/game/level/AIManager.java
@@ -1,22 +1,36 @@
 package com.catinthedark.game.level;
 
-import entity.Bullet;
-import entity.Entity;
-import entity.MushroomedCrab;
-
 import java.util.List;
+
+import com.badlogic.gdx.physics.box2d.BodyDef;
+import com.badlogic.gdx.physics.box2d.CircleShape;
+import com.catinthedark.game.physics.PhysicsModel;
+
+import entity.Bullet;
+import entity.Mushroom;
+import entity.MushroomedCrab;
 
 /**
  * Created by Ilya on 24.08.2014.
  */
 public class AIManager {
-    public void update(Level level) {
-        List<MushroomedCrab> entities = level.getCrabs();
-        for (Entity entity : entities) {
-            Bullet bullet = entity.shot();
-            if (bullet != null) {
-                level.addBullet(bullet);
-            }
-        }
-    }
+	public void update(Level level) {
+		List<MushroomedCrab> entities = level.getCrabs();
+		for (MushroomedCrab entity : entities) {
+			Bullet bullet = entity.shot();
+			if (bullet != null) {
+
+				CircleShape playerShape = new CircleShape();
+				playerShape.setRadius(0.25f);
+				PhysicsModel playerModel = new PhysicsModel(level.getWorld(),
+						entity.getBody().getPosition().x + 2, entity.getBody()
+								.getPosition().y,
+						playerShape,
+						true, BodyDef.BodyType.DynamicBody, 0.1f);
+				((Mushroom) bullet).model = playerModel;
+				playerModel.getBody().applyLinearImpulse(1, 0, 0, 0, true);
+				level.addBullet(bullet);
+			}
+		}
+	}
 }
