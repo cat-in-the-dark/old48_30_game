491dd1aed50d7859fe4829e3f7357838f760c662
Ilya Siganov <ilya.blan4@gmail.com>
2014-08-24 17:27:19 +0700

    fix score position

--- core/src/com/catinthedark/game/LDGame.java
+++ core/src/com/catinthedark/game/LDGame.java
@@ -3,6 +3,7 @@ package com.catinthedark.game;
 import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Input;
 import com.catinthedark.game.assets.Assets;
+import com.catinthedark.game.screen.GameEndScreen;
 import com.catinthedark.game.screen.ScreenChain;
 import com.catinthedark.game.screen.TitleScreen;
 import com.catinthedark.game.screen.impl.GameScreen;
@@ -23,26 +24,10 @@ public class LDGame extends Game {
         TitleScreen startGame = new TitleScreen(conf, Assets.textures.startGameTex, 0);
         startGame.bindings.bindNext(Input.Keys.ENTER);
 
-        TitleScreen gameOver = new TitleScreen(conf, Assets.textures.gameOverTex, 0){
-            @Override
-            public void render(float delta){
-                super.render(delta);
-                spriteBatch.begin();
-                Assets.fonts.hudFont.draw(spriteBatch, "Score: " + GameScore.getInstance().getScore(), conf.VIEW_PORT_WIDTH, conf.VIEW_PORT_HEIGHT);
-                spriteBatch.end();
-            }
-        };
+        TitleScreen gameOver = new GameEndScreen(conf, Assets.textures.gameOverTex, 0);
         gameOver.bindings.bindNext(Input.Keys.ESCAPE);
 
-        TitleScreen gameWin = new TitleScreen(conf, Assets.textures.winTex, 0){
-            @Override
-            public void render(float delta){
-                super.render(delta);
-                spriteBatch.begin();
-                Assets.fonts.hudFont.draw(spriteBatch, "Score: " + GameScore.getInstance().getScore(), conf.VIEW_PORT_WIDTH, conf.VIEW_PORT_HEIGHT);
-                spriteBatch.end();
-            }
-        };
+        TitleScreen gameWin = new GameEndScreen(conf, Assets.textures.winTex, 0);
         gameWin.bindings.bindNext(Input.Keys.ESCAPE);
 
 		chain.add(new TitleScreen(conf, Assets.textures.logoTex, 1000));
